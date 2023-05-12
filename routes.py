fe0028957e08cd724e9a8253752654fc72ca7488
DanialGhobrial <132240991+DanialGhobrial@users.noreply.github.com>
2023-05-12 12:10:48 +1200

    Initial Upload

---
+++ routes.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html",title="Home")
+
+@app.route('/about')
+def about():
+    return render_template("about.html",title="About")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html",title="Contact")
+
+@app.route('/movies')
+def movies():
+    return render_template("movies.html",title="movies")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
