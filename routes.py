575ab46ee867a7fe59848843846f9d35b7eff73f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-06-14 10:25:07 +1200

    Worked on making data look nicer on site

--- routes.py
+++ routes.py
@@ -27,5 +27,18 @@ def movies():
     movies = cur.fetchall()
     return render_template('movies.html', movies=movies)
 
+@app.route('/movies_detail/<int:id>')
+def movies_detail(id):
+    conn = sqlite3.connect('Database/final.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Movie WHERE id=?',(id,))
+    Movie = cur.fetchone()
+    cur.execute('SELECT * FROM Movie WHERE id=?',(movie[4],))
+    genre = cur.fetchone()
+    cur.execute('SELECT * FROM Movie WHERE id=?',(movie[3],))
+    director = cur.fetchone()
+    return render_template('movies_detail.html', movie=movie,genre=genre,description=description)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
