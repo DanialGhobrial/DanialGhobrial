7898bdf2b96355c0b3e579be3c149b3ab5943e67
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-18 09:25:55 +1200

    Fixed up all the PEP8 Requirments and started looking into reviews

--- routes.py
+++ routes.py
@@ -1,22 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
+# This is the route for the home page
+
 
 @app.route('/')
 def home():
-    return render_template("home.html",title="Home")
+    return render_template("home.html", title="Home")
+
+# This is the route for the about page
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html",title="About")
+    return render_template("about.html", title="About")
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html",title="Contact")
+    return render_template("contact.html", title="Contact")
 
 
 @app.route('/movies')
@@ -27,20 +31,21 @@ def movies():
     movies = cur.fetchall()
     return render_template('movies.html', movies=movies)
 
+
 @app.route('/movie_detail/<int:id>')
 def movies_detail(id):
     conn = sqlite3.connect('Database/final.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM Movie WHERE id=?',(id,))
+    cur.execute('SELECT * FROM Movie WHERE id=?', (id,))
     movie = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?',(movie[1],))
+    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[1],))
     genre = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?',(movie[3],))
+    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[3],))
     director = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?',(movie[4],))
+    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[4],))
     image = cur.fetchone()
-    cur.execute('SELECT * FROM movie WHERE id=?',(movie[5],))
-    return render_template('movie_detail.html', movie=movie,genre=genre,director=director,image=image)
+    cur.execute('SELECT * FROM movie WHERE id=?', (movie[5],))
+    return render_template('movie_detail.html', movie=movie, genre=genre, director=director, image=image)
 
 
 if __name__ == "__main__":
