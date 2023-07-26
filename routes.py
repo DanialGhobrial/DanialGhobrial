e3fa8a3973ee9895885a33e4e3f9e34f32bc1cb1
20351@burnside.school.nz <20351@burnside.school.nz>
2023-07-26 19:57:53 +1200

    Fixed reviews! Fixed css! Website almost complete

--- routes.py
+++ routes.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template, request, redirect
-from random import randint
-random_id = randint(1,24)
 import sqlite3
 
 app = Flask(__name__)
 
-
-
+# Function to get random data from the 'Movie' table
 def get_random_data():
-    conn = sqlite3.connect("Database/final.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Movie ORDER BY RANDOM() LIMIT 5")  # Change 'your_table' to your actual table name
-    data = cursor.fetchall()
-    conn.close()
+    with sqlite3.connect("Database/final.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Movie ORDER BY RANDOM() LIMIT 5")  # Change 'your_table' to your actual table name
+        data = cursor.fetchall()
     return data
 
 # This is the route for the home page
@@ -21,61 +17,64 @@ def home():
     random_data = get_random_data()
     return render_template('home.html', data=random_data)
 
-
-
 # This is the route for the about page
-
-
 @app.route('/about')
 def about():
     return render_template("about.html", title="About")
-  
 
-
+# This is the route for the contact page
 @app.route('/contact')
 def contact():
     return render_template("contact.html", title="Contact")
 
+# Route to add a new review for a movie
 @app.route('/movies', methods=['POST'])
 def add_movies():
-    conn = sqlite3.connect('Database/final.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (id,))
-    movies = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[0],))
     name = request.form['name']
     review = request.form['review']
     rating = request.form['rating']
     movie_id = request.form['movie_id']
-    conn = sqlite3.connect('Database/Final.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Review (name, rating, review, movie_id) VALUES (?, ?, ?, ?)', (name, rating, review,movie_id))
-    conn.commit()
-    return redirect("http://127.0.0.1:5000/movies",movies=movies)
 
+    print(f"Name: {name}, Review: {review}, Rating: {rating}, Movie ID: {movie_id}")  # Debugging line
+
+    with sqlite3.connect('Database/Final.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO Review (name, rating, review, movie_id) VALUES (?, ?, ?, ?)', (name, rating, review, movie_id))
+        conn.commit()
+
+    return redirect("/movie_detail/{}".format(movie_id))
+
+# Route to display a list of movies
 @app.route('/movies')
 def movies():
-    conn = sqlite3.connect('Database/Final.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Movie')
-    movies = cur.fetchall()
+    with sqlite3.connect('Database/Final.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Movie')
+        movies = cur.fetchall()
+
     return render_template('movies.html', movies=movies)
 
-
+# Route to display the details of a specific movie
 @app.route('/movie_detail/<int:id>')
 def movies_detail(id):
-    conn = sqlite3.connect('Database/final.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (id,))
-    movie = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[1],))
-    genre = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[3],))
-    director = cur.fetchone()
-    cur.execute('SELECT * FROM Movie WHERE id=?', (movie[4],))
-    image = cur.fetchone()
-    cur.execute('SELECT * FROM movie WHERE id=?', (movie[5],))
-    return render_template('movie_detail.html', movie=movie, genre=genre, director=director, image=image)
+    with sqlite3.connect('Database/final.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM movie WHERE id=?', (id,))
+        movie = cur.fetchone()
+
+        cur.execute('SELECT * FROM movie WHERE id=?', (movie[3],))
+        genre = cur.fetchone()
+
+        cur.execute('SELECT * FROM movie WHERE id=?', (movie[4],))
+        director = cur.fetchone()
+
+        cur.execute('SELECT * FROM movie WHERE id=?', (movie[5],))
+        image = cur.fetchone()
+
+        cur.execute('SELECT * FROM Review WHERE movie_id=?', (id,))
+        reviews = cur.fetchall()
+
+    return render_template('movie_detail.html', movie=movie, genre=genre, director=director, image=image, reviews=reviews)
 
 
 if __name__ == "__main__":
